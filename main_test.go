a0ae590d4cdc56677e2c4667920e6492e9a49b58
wesley.pereira_south <wesley.pereira@southsystem.com.br>
2022-03-03 19:26:56 -0300

    Iniciando com assert do testify

--- main_test.go
+++ main_test.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jimmmisss/go-api-gin-gorm/controllers"
+	"github.com/stretchr/testify/assert"
 )
 
 func SetupRotasTest() *gin.Engine {
@@ -20,7 +22,8 @@ func TesteVerificaStatusCodeSaudacaoComParametro(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/isa", nil)
 	resposta := httptest.NewRecorder()
 	r.ServeHTTP(resposta, req)
-	if resposta.Code != http.StatusOK {
-		t.Fatalf("Status error: valor recebido foi %d e o valor esperado era %d", resposta.Code, http.StatusOk)
-	}
+	assert.Equal(t, http.StatusOK, resposta.Code, "deveriam ser iguais")
+	mockResposta := `{"API diz":"E ai isa, Tudo blz"}`
+	respostaBody, _ := ioutil.ReadAll(resposta.Body)
+	assert.Equal(t, mockResposta, respostaBody)
 }
