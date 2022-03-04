3bffd266854b03eb52d87db08c95dcff85c94da0
wesley.pereira_south <wesley.pereira@southsystem.com.br>
2022-03-04 10:52:05 -0300

    Web mvc

--- routes/routes.go
+++ routes/routes.go
@@ -8,12 +8,16 @@ import (
 func HandleRequests() {
 	addr := "127.0.0.1:8080"
 	r := gin.Default()
-	
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./assets")
+	r.NoRoute(controllers.RotaNaoEncontrada)
+
 	r.GET("/:nome", controllers.Saudacao)
 	r.POST("/:criar", controllers.CriarNovoAluno)
 	r.GET("/alunos/:id", controllers.BucaAlunoPorId)
 	r.DELETE("/alunos/:id", controllers.DeletaAluno)
 	r.PATCH("/alunos/:id", controllers.EditaAluno)
 	r.GET("/alunos/:cpf", controllers.BuscaAlunoPorCPF)
+	r.GET("/index", controllers.ExibePaginaIndex)
 	r.Run(addr)
 }
